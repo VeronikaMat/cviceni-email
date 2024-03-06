7a69ccbf624191430765277b87260bcf78bae5b5
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-06 20:30:36 +0100

    reapair code HTML and JS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-function goodbye(jmeno) {
+/*function goodbye(jmeno) {
  const rozlouceni = document.querySelector('.email__closing')   
     if (jmeno) {
         rozlouceni.textContent = `S pozdravem ${jmeno}`
@@ -11,4 +11,29 @@ function goodbye(jmeno) {
     
   
   // Zavoláme funkci s příkladovým jménem
-  goodbye('Pavel Ovesný')
+  goodbye('Pavel Ovesný')*/
+
+
+  function goodbye(name) {
+    const closingParagraph = document.querySelector('.email__closing');
+    if (name) {
+      closingParagraph.textContent = `S pozdravem ${name}`;
+    } else {
+      closingParagraph.textContent = 'Na shledanou';
+    }
+  }
+  
+  function fillSubject(subject) {
+    const subjectElement = document.querySelector('.email__subject');
+    subjectElement.textContent = subject;
+  }
+  
+  function fillBody(body, name) {
+    const bodyElement = document.querySelector('.email__body');
+    bodyElement.textContent = body;
+    goodbye(name);
+  }
+  
+  // Testování funkcí
+  fillSubject('Nová nabídka práce');
+  fillBody('Toto je obsah e-mailu.', 'Pavel Ovesný');
