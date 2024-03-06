ad9758dbd8d918b1619fe1a1a71c84adda9e2c02
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-06 19:10:51 +0100

    add code JS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function goodbye(jmeno) {
+    const closingParagraph = document.querySelector('.email__closing');
+    if (jmeno) {
+      closingParagraph.textContent = `S pozdravem ${jmeno}`;
+    } else {
+      closingParagraph.textContent = 'Na shledanou';
+    }
+  }
+  
+  // Zavoláme funkci s příkladovým jménem
+  goodbye('Pavel Ovesný');
